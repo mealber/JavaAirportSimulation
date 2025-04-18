2cd72b578e9e9e44e5119553c6d534fc69dafe38
mariah alber <mealber@asu.edu>
2025-04-17 20:51:25 -0700

    Created abstract creator for airplanes using factory method DP

---
+++ src/main/java/AirplaneCreator.java
@@ -0,0 +1,11 @@
+public abstract class AirplaneCreator{
+    public abstract Airplane createAirplane();
+
+    public Airplane prepareAirplane(String destination, int gate, int departureTime){
+        Airplane plane = createAirplane();
+        plane.setDestination(destination);
+        plane.setGate(gate);
+        plane.setDepartureTime(departureTime);
+        return plane;
+    }
+}
