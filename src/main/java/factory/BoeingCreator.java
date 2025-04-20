e144afdf8cdfa42eabf0cd1d17736bf59024cd4b
mariah alber <mealber@asu.edu>
2025-04-19 18:07:55 -0700

    Renamed factorymethod folder to fix case issue on github

--- src/main/java/factory/BoeingCreator.java
+++
@@ -1,9 +0,0 @@
-package factorymethod;
-
-public class BoeingCreator extends AirplaneCreator {
-    @Override
-    public Airplane createAirplane() {
-        Boeing boeingPlane = new Boeing();
-        return boeingPlane;
-    }
-}
