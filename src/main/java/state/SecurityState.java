f5aa1009e062e36db4ef6ffe4ae12aa05d614126
mariah alber <mealber@asu.edu>
2025-04-19 17:41:04 -0700

    Created SecurityState class to handle logic for passenger security checkpoints

---
+++ src/main/java/state/SecurityState.java
@@ -0,0 +1,19 @@
+import factorymethod.Airport;
+
+public class SecurityState implements PassengerState {
+    @Override
+    public boolean handle(Passenger passenger, Airport airport) {
+        for (int i = 0; i < airport.getSecurityChecks(); i++) {
+            double failingChance = Math.random();
+            if (failingChance < 0.05) { //5% chance to fail security check points
+                System.out.println(passenger.getName() + " failed security checkpoint " + (i + 1) + ".");   
+                return false;
+            }
+
+            System.out.println(passenger.getName() + " passed security checkpoint " + (i + 1) + ".");
+        }
+
+        //passenger.setState(new BoardingState()); //change state if all security points are passed
+        return true;
+    }
+}
