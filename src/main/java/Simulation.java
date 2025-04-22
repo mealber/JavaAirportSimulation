650aeca4fdfbc791c2f26fdb278c30d9d4be339f
mariah alber <mealber@asu.edu>
2025-04-21 21:33:20 -0700

    Starting on temporary implementation of simulation to test functionality of design patterns

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,9 +1,30 @@
+import java.util.ArrayList;
+import factory.*;
+import state.*;
+import data.*;
+
 public class Simulation {
-    //create list of names for passengers to randomly pull from 
-    //(this should probably be in a PassengerName/ PassengerInfo class of some sort??)
-    //create airports and add to airport list
-    //create airplanes and add to airplane list, 
-    //enough for multiple airplanes per airport
-    //randomly assign airplanes to airports
-    //create passengers using random names and assign them to flights
+
+    public void setUp() {
+        AirplaneCreator airplaneCreator = new BoeingCreator();
+        AirportCreator airportCreator = new LaxAirportCreator();
+
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<Airport> airports = new ArrayList<>();
+        ArrayList<Airplane> airplanes = new ArrayList<>();
+        ArrayList<Flight> flights = new ArrayList<>();
+
+        names.add("Bob");
+        names.add("Sarah");
+
+        Airport lax = airportCreator.createAirport();
+        airports.add(lax);
+
+        Airplane boeing = airplaneCreator.prepareAirplane("Paris", 3, 1200);
+        airplanes.add(boeing);
+
+        flights.add(boeing.toFlight());
+
+        Passenger p1 = new Passenger(names.get(0),flights.get(0));
+    }
 }
