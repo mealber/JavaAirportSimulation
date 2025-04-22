2ccd08a9636b202314042d91dc8535d8841466c8
mariah alber <mealber@asu.edu>
2025-04-21 20:59:31 -0700

    Temporary simulation class with pseudo code

---
+++ src/main/java/Simulation.java
@@ -0,0 +1,9 @@
+public class Simulation {
+    //create list of names for passengers to randomly pull from 
+    //(this should probably be in a PassengerName/ PassengerInfo class of some sort??)
+    //create airports and add to airport list
+    //create airplanes and add to airplane list, 
+    //enough for multiple airplanes per airport
+    //randomly assign airplanes to airports
+    //create passengers using random names and assign them to flights
+}
