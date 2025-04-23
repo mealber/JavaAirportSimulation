1859e284a569614f8266408eacf8c3e22638fa09
mariah alber <mealber@asu.edu>
2025-04-22 22:35:10 -0700

    deleted uneeded simulation class and updated main to use new SimulatoinPreparer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import data.*;
+
 public class Main {
     /**
     *entry point of airport simulation.
     */
     public static void main(String[] args) {
-        Simulation simulation = new Simulation();
+        SimulationPreparer simulation = new SimulationPreparer();
         simulation.setUp();
     }
 }
