7fad144af68c8cc92682ba68d9a42da3f60324a4
mariah alber <mealber@asu.edu>
2025-04-24 02:48:33 -0700

    Implemented airport observer

--- src/main/java/observer/AirportObserver.java
+++ src/main/java/observer/AirportObserver.java
@@ -1,15 +1,33 @@
 package observer;
 
-import factory.Airport;
+import factory.*;
 
 public class AirportObserver implements Observer {
+    Simulation simulation;
+    Airport airport;
 
-    public AirportObserver() {
-
+    public AirportObserver(Simulation s) {
+        this.simulation = s;
+        airport = null; //no airport yet
     }
 
     @Override
     public void update() {
-        //todo
+        airport = simulation.getAirport(); //update airport to current airport
+        System.out.println("Simulation Round " + simulation.getRound() + ":");
+        System.out.println("---------------------------------------------");
+
+        //display airport resources
+        System.out.println("Current Airport: " + airport.getName());
+        System.out.println("Gates Available: " + airport.getGates());
+        System.out.println("Runways Available: " + airport.getRunways());
+        System.out.println("Security Checks Required: " + airport.getSecurityChecks());
+        System.out.println("---------------------------------------------");
+
+        //display airplanes and gate assignments
+        for(Airplane a : simulation.getAirplanes()) {
+            System.out.println(a.getName() + " has landed at " + airport.getName() + " Airport. Gate Assignment: " + a.getGate());
+        }
+        System.out.println("---------------------------------------------");
     }
 }
