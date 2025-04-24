a8c4fc8f95c22b33c8731e4d3658272cbf6574b1
mariah alber <mealber@asu.edu>
2025-04-24 04:09:54 -0700

    Added new observer

---
+++ src/main/java/observer/RoundObserver.java
@@ -0,0 +1,14 @@
+package observer;
+
+public class RoundObserver implements Observer {
+    Simulation s;
+
+    public RoundObserver(Simulation s) {
+        this.s = s;
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
